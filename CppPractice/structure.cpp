be3c09ba525c62cb8155507f048a639b78f0fa04
maxdubnowski <madubnowski@gmail.com>
2023-06-01 10:17:18 -0500

    Structures and also string streams

---
+++ CppPractice/structure.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+struct movies{
+  string title;
+  int cost;
+  int year;
+} mine, yours;
+
+void printMovie(movies movie){
+  cout << movie.title << " ("<< movie.year << ") - $" <<movie.cost << endl;;
+}
+
+int main (){
+  string mystr;
+  mine.title = "The Joker";
+  mine.year = 2018;
+  mine.cost = 12;
+
+  cout << "Enter your Favorite Title: " ;
+  getline (cin, yours.title);
+  cout << "Enter year: ";
+  getline(cin, mystr);
+  stringstream(mystr) >> yours.year;
+  
+  cout << "Enter cost: ";
+  cin >> mystr;
+  stringstream(mystr) >> yours.cost;
+
+  cout << "My favorite movie is: " <<endl;
+  printMovie(mine);
+
+  cout << "Yours is: " <<endl;
+  printMovie(yours);
+  
+
+  return 0;
+}
+
